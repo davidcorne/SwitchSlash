992fbfc0c3f7873b232a6f207e027199f0e36671
David Corne <david.corne@autodesk.com>
2019-09-17 13:34:09 +0100

    Print the switches slashes for now

--- source/Main.cs
+++ source/Main.cs
@@ -2,18 +2,24 @@
 //
 
 using System;
-using System.Collections.Generic;
+using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 //=============================================================================
 namespace SS
 {
     class Run
     {
+        [STAThread]
         static void Main()
         {
-            Console.WriteLine("SS");
+            string text = Clipboard.GetText();
+            // Swap all / for \ and visa-versa
+            char[] buffer = text.Select(c => c == '\\' ? '/' : (c == '/' ? '\\' : c)).ToArray();
+            string newText = new string(buffer);
+            Console.WriteLine(newText);
         }
     }
 }
